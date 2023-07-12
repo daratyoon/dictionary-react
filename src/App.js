16489678473b96489ad981e4082332416254b7d0
daratyoon <sandratsaiiyun@yahoo.com>
2023-07-12 18:54:55 -0500

    Created new component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div class="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer>
+        <small>
+          Coded by <a href="https://sandra-tsai-yun.netlify.app/">Sandra</a>
+        </small>
+      </footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState(" ");
+
+  function search(event) {
+    event.preventDefault();
+    alert("Searching");
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} />
+      </form>
+    </div>
+  );
+}
