d68a487aedfa45109bf2f81e126319a7b927f5c3
daratyoon <sandratsaiiyun@yahoo.com>
2023-07-18 15:59:12 -0500

    Created a new component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results.js";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  let [keyword, setKeyword] = useState(" ");
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState("");
 
   function handleResponse(response) {
-    console.log(response);
+    setResults(response.data);
   }
 
   function search(event) {
@@ -28,6 +30,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
---
+++ src/Results.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Results(props) {
+  return <div className="Results">Hi from Results</div>;
+}
