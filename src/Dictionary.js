9d96b69e334087ca3ba3e6a5e086755411343a8e
daratyoon <sandratsaiiyun@yahoo.com>
2023-07-17 19:54:32 -0500

    Installed Axios and integrated API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(" ");
 
+  function handleResponse(response) {
+    console.log(response);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Seaching for the definition for: "${keyword}"`);
+
+    let apiKey = "a432b612baftbo7e8e8c94493b9d5ee0";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=book&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
