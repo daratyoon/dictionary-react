994e5156d0a838ec2a43b5481bdadd86e3d461c1
daratyoon <sandratsaiiyun@yahoo.com>
2023-07-18 20:30:31 -0500

    Looped array of objects

--- src/Results.js
+++ src/Results.js
@@ -3,7 +3,14 @@ import React from "react";
 export default function Results(props) {
   console.log(props.results);
   if (props.results) {
-    return <div className="Results">Hello</div>;
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function (meaning, index) {
+          return meaning.definition;
+        })}
+      </div>
+    );
   } else {
     return null;
   }
