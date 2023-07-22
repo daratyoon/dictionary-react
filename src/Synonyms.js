681dffa462af8bb0c3c9c39fad3c8c679b04d652
daratyoon <sandratsaiiyun@yahoo.com>
2023-07-22 13:05:55 -0500

    Looped all available synonyms and created an if statement

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,7 +2,14 @@ import React from "react";
 
 export default function Synonyms(props) {
   if (props.synonyms) {
-    return <div className="Synonyms">Hello from synonyms</div>;
+    return (
+      <div className="Synonyms">
+        Synonyms:
+        {props.synonyms.map(function (synonym, index) {
+          return <ul key={index}>{synonym}</ul>;
+        })}
+      </div>
+    );
   } else {
     return null;
   }
