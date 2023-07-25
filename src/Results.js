45e773a46e82fe0ced05fa3768cc19bf19d6fe67
daratyoon <sandratsaiiyun@yahoo.com>
2023-07-25 00:05:30 -0500

    Added phonetic feature

--- src/Results.js
+++ src/Results.js
@@ -7,6 +7,8 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2 className="searchedWord">{props.results.word}</h2>
+        <p className="wordPhonetic">{props.results.phonetic}</p>
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
